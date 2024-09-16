916746bc4004129af40da85b65703efe201b37d4
daichi0918 <kayasugadaichi@icloud.com>
2024-09-16 12:54:27 +0900

    途中まで

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 import reportWebVitals from './reportWebVitals';
+import { TodoProvider } from './contexts/TodoContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoProvider>
+      <RouterProvider router={router}/>
+    </TodoProvider>
   </React.StrictMode>
 );
 
